2030bdfd724a06b18e76b7f97322409850a1ccf8
renaneliakim1 <renaneliakim1@gmail.com>
2025-08-07 16:59:38 -0300

    feat: add urls

---
+++ tasks/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'tasks'
+
+urlpatterns = [
+    path('', views.task_list, name='list'),
+    path('create/', views.task_create, name='create'),
+    path('<int:pk/toggle', views.task_toggle, name='toggle'),
+    path('<int:pk/edit/', views.task_edit, name='edit'),
+    path('<int:pk/delete/', views.task_delete, name='delete'),
+
+]
