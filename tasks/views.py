ad642ba79ac41b0ef75ef449e45e8d5dd576b99a
renaneliakim1 <renaneliakim1@gmail.com>
2025-08-08 17:50:29 -0300

    fix

--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect , get_object_or_404
 
-from .models import Tasks
+from .models import Task
 from .forms import TaskForm
 
 def task_list(request):
-    tasks = Tasks.objects.order_by('-created_at')
+    tasks = Task.objects.order_by('-created_at')
 
-    return render(request, 'task/task_list.html', {'tasks': tasks})
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
 def task_create(request):
     if request.method == 'POST':
-        form = TasksForm(request.POST)
+        form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('tasks:list)')
@@ -19,14 +19,14 @@ def task_create(request):
         return render(request, 'task/task_form.html', {'form' : form, 'title': 'Nova Tarefa'})
 
 def task_toggle(request, pk): 
-    task = get_object_or_404(Tasks, pk=pk)
+    task = get_object_or_404(Task, pk=pk)
     task.done = not task.done
     task.save()
     return redirect('tasks:list')
 
 
 def task_edit(request, pk): 
-    task = get_object_or_404(Tasks, pk=pk)
+    task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
@@ -38,8 +38,8 @@ def task_edit(request, pk):
     
 
 def task_delete(request, pk): 
-    task = get_object_or_404(Tasks, pk=pk)
+    task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
         task.delete()
         return redirect('tasks:list')
-    return render(request, 'task/task_confirm_delete.html', {'task': task})
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
